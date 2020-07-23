0c2bffe9d03c9a368caf651fe1e0cb5556d491cc
monyarm <monyarm@gmail.com>
2020-07-23 22:05:24 +0300

    fixed setcodes

--- script/c19710001.lua
+++ script/c19710001.lua
@@ -2,4 +2,12 @@
 local s, id = GetID()
 function s.initial_effect(c)
   -- effects
+
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_ADD_SETCODE)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE+EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e1:SetRange(LOCATION_ALL)
+	e1:SetValue(0xa008c008)
+	c:RegisterEffect(e1)
 end
--- script/c19710002.lua
+++ script/c19710002.lua
@@ -10,9 +10,19 @@ function s.initial_effect(c)
 	e0:SetType(EFFECT_TYPE_SINGLE)
 	e0:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
 	e0:SetCode(EFFECT_ADD_RACE)
-	e0:SetRange(LOCATION_MZONE)
+	e0:SetRange(LOCATION_ALL)
 	e0:SetValue(RACE_WARRIOR)
 	c:RegisterEffect(e0)
+
+	--SetCode
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_ADD_SETCODE)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE+EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e1:SetRange(LOCATION_ALL)
+	e1:SetValue(0xa008)
+	e1:SetValue(0x3008)
+	c:RegisterEffect(e1)
 
 
 	--Atk Change
