3815d9e766feb9c59bd91a23cd38fd0d62ac4905
monyarm <monyarm@gmail.com>
2020-07-26 14:41:43 +0300

    kamen ride fix

--- script/c19710014.lua
+++ script/c19710014.lua
@@ -1,49 +1,43 @@
--- Kamen Ride - Decadelocal s,id=GetID()
+-- Kamen Ride - Decade
+local s,id=GetID()
 function s.initial_effect(c)
   --Activate
-	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e1:SetType(EFFECT_TYPE_ACTIVATE)
-	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetHintTiming(0,TIMING_END_PHASE)
-	e1:SetTarget(s.target)
-	e1:SetOperation(s.activate)
-	c:RegisterEffect(e1)
+  local e1=Effect.CreateEffect(c)
+  e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+  e1:SetType(EFFECT_TYPE_ACTIVATE)
+  e1:SetCode(EVENT_FREE_CHAIN)
+  e1:SetCost(s.cost)
+  e1:SetTarget(s.target)
+  e1:SetOperation(s.activate)
+  c:RegisterEffect(e1)
 end
 s.listed_names={19710009}
 s.listed_series={0xa008}
-
-function s.tfilter(c,att,e,tp,mc)
-	return c:IsCode(19710009)  and c:IsAttribute(att) and Duel.GetLocationCountFromEx(tp,tp,mc,c)>0 and c:IsCanBeSpecialSummoned(e,0,tp,true,false)
+function s.cfilter(c,ft,tp)
+  return c:IsSetCard(0xa008) and (ft>0 or (c:GetSequence()<5 and c:IsControler(tp))) and (c:IsFaceup() or c:IsControler(tp))
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+  e:SetLabel(1)
+  local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+  if chk==0 then return Duel.CheckReleaseGroupCost(tp,s.cfilter,1,false,nil,nil,ft,tp) end
+  local g=Duel.SelectReleaseGroupCost(tp,s.cfilter,1,1,false,nil,nil,ft,tp)
+  Duel.Release(g,REASON_COST)
 end
 function s.filter(c,e,tp)
-	return c:IsFaceup() and c:IsSetCard(0x8)
-		and Duel.IsExistingMatchingCard(s.tfilter,tp,LOCATION_HAND+LOCATION_DECK+LOCATION_GRAVE,0,1,nil,c:GetAttribute(),e,tp,c)
+  return c:IsCode(19710009) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
-function s.chkfilter(c,att)
-	return c:IsFaceup() and c:IsSetCard(0xa008)
-end
-function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_MZONE) and s.chkfilter(chkc,e:GetLabel()) end
-	if chk==0 then return Duel.IsExistingTarget(s.filter,tp,LOCATION_MZONE,0,1,nil,e,tp) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-	local g=Duel.SelectTarget(tp,s.filter,tp,LOCATION_MZONE,0,1,1,nil,e,tp)
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND+LOCATION_DECK+LOCATION_GRAVE)
-	e:SetLabel(g:GetFirst():GetAttribute())
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+  if chk==0 then
+      if e:GetLabel()==0 and Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return false end
+      e:SetLabel(0)
+      return Duel.IsExistingMatchingCard(s.filter,tp,0x13,0,1,nil,e,tp) end
+  Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,0x13)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
-	local tc=Duel.GetFirstTarget()
-	if not tc:IsRelateToEffect(e) then return end
-	local att=tc:GetAttribute()
-	if Duel.SendtoGrave(tc,REASON_EFFECT)==0 then return end
-	local g=Duel.GetMatchingGroup(s.tfilter,tp,LOCATION_HAND+LOCATION_DECK+LOCATION_GRAVE,0,nil,att,e,tp)
-	if #g==0 then return end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local sg=g:Select(tp,1,1,nil)
-	if #sg>0 then
-		Duel.BreakEffect()
-		Duel.SpecialSummon(sg,0,tp,tp,true,false,POS_FACEUP)
-		sg:GetFirst():CompleteProcedure()
-	end
+  if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+  Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+  local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.filter),tp,0x13,0,1,1,nil,e,tp)
+  if #g>0 then
+      Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+  end
 end
